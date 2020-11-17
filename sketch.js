f2a85235435aa692cdc24b552a444ecb8c588b08
27bslash <greenpear68@yahoo.co.uk>
2020-11-17 15:27:47 +0000

    faces drawn

--- sketch.js
+++ sketch.js
@@ -1,13 +1,58 @@
-
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  frameRate(10);
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(200);
+  let sqWidth = 100;
+  for (let i = 0; i < cube.length; i++) {
+    rectMode(CORNER);
+    // rect(i * 305, 0, 300);
+
+    for (let j = 0; j < cube[i].length; j++) {
+      rectMode(CORNER);
+      let y;
+      if (j < 3) {
+        y = 0;
+      } else if (j < 6) {
+        y = 100;
+      } else if (j < 9) {
+        y = 200;
+      }
+      fill(cube[i][j]);
+      rect((j % 3) * sqWidth + i * 305, y, sqWidth);
+    }
+  }
 }
 
 function draw() {
-  background(200);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  fill("blue");
-
-  box(500);
+  drawCube();
+  // scramble();
+  // solveXd()
+  // rotateX(frameCount * 0.01);
+  // rotateY(frameCount * 0.01);
+  // fill("blue");
+  // box(500);
 }
+const drawCube = () => {
+  let sqWidth = 100;
+  for (let i = 0; i < cube.length; i++) {
+    rectMode(CORNER);
+    let sideText = ["Top", "Face", "Bottom", "Back", "Right", "Left"];
+    fill("black");
+    textSize(30);
+    text(sideText[i], 113 + i * 305, 350);
+    for (let j = 0; j < cube[i].length; j++) {
+      rectMode(CORNER);
+      let y;
+      if (j < 3) {
+        y = 0;
+      } else if (j < 6) {
+        y = 100;
+      } else if (j < 9) {
+        y = 200;
+      }
+      // console.log(y, (j % 3) * 100 + i * 305);
+      fill(cube[i][j]);
+      rect((j % 3) * sqWidth + i * 305, y, sqWidth);
+    }
+  }
+};
